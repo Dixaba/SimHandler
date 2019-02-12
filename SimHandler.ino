da5f4d4510aee25dd7bf693711763a08c367c898
Dixaba <dixaba@yandex.ru>
2019-02-12 17:47:23 +0300

    Вывод сообщений на экран

--- SimHandler.ino
+++ SimHandler.ino
@@ -1,16 +1,53 @@
-#include <timer-api.h>
 #include <LiquidCrystal_I2C.h>
+#include <timer-api.h>
 #include <SoftwareSerial.h>
 
-// Configure software serial port
+#define btn 4
+#define BLT 3
+
 SoftwareSerial SIM900(7, 8);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+volatile bool nextState = false;
+byte state = 0;
 
 void timer_handle_interrupts(int timer)
 {
+  static int lastBtnState = HIGH;
+  static int currBtnState = HIGH;
+  int btnState = digitalRead(btn);
+  static int btnCount = 0;
+  static int count = 1000;
+
+  if (btnState != lastBtnState)
+    {
+      btnCount = 50;
+    }
+
+  if (btnCount > 0)
+    {
+      btnCount--;
+    }
+  else
+    {
+      if (btnState != currBtnState)
+        {
+          currBtnState = btnState;
+
+          if (currBtnState == LOW)
+            {
+              nextState = true;
+            }
+        }
+    }
+
+  lastBtnState = btnState;
 }
 
 void setup()
 {
+  timer_init_ISR_1KHz(TIMER_DEFAULT);
+  lcd.init();
   delay(3000);
   Serial.begin(19200);
   SIM900.begin(19200);
@@ -39,6 +76,13 @@ void loop()
 
       input += in;
     }
+
+  if (nextState)
+    {
+      state++;
+      lcd.print(state);
+      nextState = false;
+    }
 }
 
 void handleSimMessage(String input)
@@ -49,12 +93,18 @@ void handleSimMessage(String input)
     {
       if (input.indexOf("PLAY TONE") > 0)
         {
+          lcd.clear();
+          lcd.print("Play tone");
+          lcd.backlight();
           Serial.println("play tone");
           SIM900.print("AT*PSSTK=\"PLAY TONE\",1,0\r");
         }
       else
         if (input.indexOf("DISPLAY TEXT") > 0)
           {
+            lcd.clear();
+            lcd.print("Play tone");
+            lcd.backlight();
             Serial.println("display text");
             SIM900.print("AT*PSSTK=\"DISPLAY TEXT\",1,0\r");
           }
@@ -67,15 +117,29 @@ void handleSimMessage(String input)
           else
             if (input.indexOf("NOTIFICATION") > 0)
               {
+                lcd.clear();
+                lcd.print("Notification");
+                lcd.backlight();
                 Serial.println("notification");
                 SIM900.print("AT*PSSTK=\"NOTIFICATION\",1,0\r");
               }
             else
               if (input.indexOf("GET INPUT") > 0)
                 {
+                  lcd.clear();
+                  lcd.print("Get input");
+                  lcd.backlight();
                   Serial.println("get input");
                   SIM900.print("AT*PSSTK=\"GET INPUT\",1,4,\"111111\",0,0\r");
                 }
+              else
+                if (input.indexOf("END SESSION") > 0)
+                  {
+                    lcd.clear();
+                    lcd.print("End session");
+                    lcd.noBacklight();
+                    Serial.println("end");
+                  }
     }
 }
 
